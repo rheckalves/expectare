2646e37b1b83b15cffceaec729b1c5e742d98b39
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2024-04-27 11:38:52 -0300

    chore: added sentry and redux-saga

--- App.tsx
+++ App.tsx
@@ -5,6 +5,11 @@ import HomeScreen from '@expectare/screens/Home';
 import Settings from '@expectare/screens/Settings';
 import Workspace from '@expectare/screens/Workspace';
 import Icon from 'react-native-vector-icons/FontAwesome6';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://cbc1e23c2b8976ba343d8fde3526f9b5@o4507158997041152.ingest.us.sentry.io/4507158997237760',
+});
 
 const Tab = createBottomTabNavigator();
 
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,9 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
+const {
+  createSentryMetroSerializer
+} = require("@sentry/react-native/dist/js/tools/sentryMetroSerializer");
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
@@ -10,6 +14,10 @@ const config = {
   resolver: {
     enableGlobalPackages: true,
   },
+
+  serializer: {
+    customSerializer: createSentryMetroSerializer()
+  }
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
